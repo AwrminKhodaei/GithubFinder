b78f0061bbb8d4aca56f906b80d576dce507a931
armin <awrminkhodaei@gmail.com>
2018-08-29 15:25:52 +0430

    changed to repo and commits count

--- src/components/UserRepos.vue
+++ src/components/UserRepos.vue
@@ -1,33 +1,26 @@
 <template>
 	<b-container>
+		<b-row v-if="isLoading" class="loader-container">
+			<ball-grid-pulse-loader class="loader" color="#56a900" size="100px"></ball-grid-pulse-loader>
+		</b-row>
 		<b-row>
-			<b-col class="mx-auto mt-5 mb-3" md="8" sm="12" v-for="(repo, index) in repos" :key="index">
+			<b-col class="mx-auto mt-5 mb-3" md="12" sm="12">
+				<h3>Showing Results Of <em class="text-primary">{{repoName}}</em> Repository For <span class="text-primary">{{username}}</span></h3>
 				<div class="repository bg-light p-5 shadow rounded">
-					<b-row>
-						<b-col md="2" sm="6" class="pl-2  repo-img">
-							<i class="fas fa-code-branch fa-3x"></i>
-						</b-col>
-						<b-col md="8" sm="6" class="text-center repo-info">
-							<h4 class="repo-name">{{repo.name}}</h4>
-							<small class="my-2 d-block">{{repo.description}}</small>
-							  <b-button-group>
-									<b-button variant="light">
-										<i class="fas fa-code-branch"></i>
-										Forks {{repo.forks}}
-									</b-button>
-									<b-button variant="light">
-										<i class="fas fa-star"></i>
-										Stars {{repo.stargazers_count}}
-									</b-button>
-									<b-button variant="light" @click="showRepo(repo.name, repo.owner.login)">
-										<i class="fas fa-users"></i>
-										Show Contributors
-									</b-button>
-								</b-button-group>
+					<b-row class="my-3 py-3">
+						<b-col class="my-4" md="3" v-for="(contributor, index) in contributors[0]" :key="index">
+							<div class="text-center p-5">
+								<a :href="contributor.author.url" target="_blank">
+								<img :src="contributor.author.avatar_url" width="60px" height="60px">
+								<p>{{contributor.author.login}}</p>
+								</a>
+								<b-btn v-b-tooltip.hover :title="contributor.total">commits</b-btn>
+							</div>
 						</b-col>
 					</b-row>
 				</div>
 			</b-col>
+			<h1 class="text-danger text-center">{{errorMassage}}</h1>
 		</b-row>
 	</b-container>
 </template>
@@ -35,20 +28,56 @@
 import config from '../../config/config.js'
 import axios from 'axios'
 export default {
-props: ['repos'],
+	props: {
+		username: {
+			type: String,
+			required: true
+		},
+		repoName: {
+			type: String,
+			required: true
+		},
+	},
 	data() {
 		return {
-			isFalse: false
+			isLoading: true,
+			contributors: [],
+			errorMassage: ''
 		}
 	},
-	methods : {
-		showRepo (repoName,ownerName) {
-			let self = this
-			axios.get(`https://api.github.com/repos/${ownerName}/${repoName}/stats/contributors`, config)
-			.then((response)=> console.log (response.data.length))
-		}
+	mounted () {
+		let self = this
+		axios.get(`https://api.github.com/repos/${this.username}/${this.repoName}/stats/contributors`, config)
+		.then(function(response){
+			self.contributors.push(response.data)
+			self.isLoading = !self.isLoading
+		}).catch(function(err){
+			self.errorMassage = 'ops something went wrong :('
+		})
 	}
 }
 </script>
 <style scoped>
+img{
+	border-radius: 50%;
+}
+.loader-container{
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%,-50%);
+	z-index: 9999;
+	background-color: #fff;
+	width: 100vw;
+	height: 100vh;
+	transition: all 500ms ease;
+}
+.loader{
+	text-align: center;
+	margin: 0 auto;
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%,-50%);
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+import 'vue-loaders/dist/vue-loaders.css';
+import * as VueLoaders from 'vue-loaders';
+Vue.use(VueLoaders);
 Vue.use(BootstrapVue)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
